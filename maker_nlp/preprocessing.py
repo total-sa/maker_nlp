261e7f3bd2039bfd19a07a12887906e7cf2ceb28
bachtn <alaa.bakhti@epita.fr>
2021-01-05 12:00:42 +0100

    apply text cleaning on the dataframe and create a Makefile for dependency installation

--- maker_nlp/preprocessing.py
+++ maker_nlp/preprocessing.py
@@ -8,20 +8,20 @@ def convert_to_lowercase(text: str) -> str:
 def remove_accents(text: str) -> str:
     return unidecode.unidecode(text)
 
-def remove_ponctuation_and_digits(text: str) -> str:
+def remove_punctuation_and_digits(text: str) -> str:
     exclist = string.punctuation + string.digits
     table_ = str.maketrans(exclist, ' ' * len(exclist))
     text = text.translate(table_)
     return text
 
 def remove_spaces_and_new_lines(text: str) -> str:
-    return re.sub('\s+',' ', text).strip()
+    return re.sub('\s+', ' ', text).strip()
     
 
 def normalize_text(text: str) -> str:
     text = convert_to_lowercase(text)
     text = remove_accents(text)
-    text = remove_ponctuation_and_digits(text)
+    text = remove_punctuation_and_digits(text)
     text = remove_spaces_and_new_lines(text)
     return text
 
@@ -32,7 +32,7 @@ def remove_stop_words(text: str, stop_words_list: list) -> str:
 
 
 def clean_text(stop_words_list: [str]):
-    def apply_to_sentence(text: str):
+    def apply_to_sentence(text: str) -> str:
         text = normalize_text(text)
         text = remove_stop_words(text, stop_words_list)
         return text
